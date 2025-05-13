29b6e4938620f31723b71d3342dfc8ca86c7f16e
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2025-05-14 00:39:24 +0200

    Punch is now made in the player nod + Elite Enemies right draw order

--- SCENES/SCRIPTS/PickupResource.gd
+++ SCENES/SCRIPTS/PickupResource.gd
@@ -4,6 +4,7 @@ class_name  Pickups
 @export var title: String
 @export var icon: Texture2D
 @export_multiline var description: String
+@export var sfx: AudioStream
 
 var player_reference: CharacterBody2D
 
--- SCENES/SCRIPTS/Player.gd
+++ SCENES/SCRIPTS/Player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 const FLOOF_PLAYER_ONE_ARM_PLACE_HOLDER = preload("res://ART/Floof Player_One Arm_Place holder.png")
 const FLOOF_PLAYER_PLACE_HOLDER = preload("res://ART/Floof Player_Place holder.png")
 
+
+
 var knockback: Vector2
 var speed: float = 200
 var health: float = 100:
@@ -32,6 +34,14 @@ var level: int = 1:
 		elif level >= 7:
 			%XP.max_value = 25
 
+#MELE ATTACK
+var attack_direction: Vector2 
+var attack_damage: float = 5
+var attack_speed: float = 100
+
+
+func _ready() -> void:
+	$"Mele Attacks/Fist Punch Area/First Punch".visible = false
 
 func _physics_process(delta):
 	camera_zoom()
@@ -45,20 +55,41 @@ func _physics_process(delta):
 	move_and_collide(velocity * delta)
 	#move_and_slide()
 	check_XP()
+	#punch_attack_inst()
+	punch_attack()
 	
 	if velocity.x < 0:
 		$Sprite2D.flip_h = false
 	elif velocity.x > 0:
 		$Sprite2D.flip_h = true
+	
+
+
+func punch_attack():
+	if Input.is_action_just_pressed("click"):
+		var animation_time =  $AnimationPlayer.get_playing_speed()
+		$"Mele Attacks/Fist Punch Area/First Punch".visible = true
+		$AnimationPlayer.play("Punch 1")
 		
+		if $AnimationPlayer.is_playing():
+			$Sprite2D.texture = FLOOF_PLAYER_ONE_ARM_PLACE_HOLDER
+		if $Sprite2D.flip_h == true:
+			$"Mele Attacks/Fist Punch Area".scale.x = -1
+		else: 
+			$"Mele Attacks/Fist Punch Area".scale.x = 1
+		await get_tree().create_timer(0.3).timeout
+		$Sprite2D.texture = FLOOF_PLAYER_PLACE_HOLDER
+	
+func punch_attack_inst():
 	if Input.is_action_just_pressed("click"):
 		$Sprite2D.texture = FLOOF_PLAYER_ONE_ARM_PLACE_HOLDER
 		var punch_attack = PUNCH.instantiate()
-		var offset =  Vector2(-150,-150)
-		if $Sprite2D.flip_h == false:
-			offset.x *= -1
+		var offset =  Vector2(150,150)
+		if $Sprite2D.flip_h == true:
+			punch_attack.set_scale(Vector2(-1, 1))
+			punch_attack.position = position + offset
 		punch_attack.position = position + offset
-		add_child(punch_attack)
+		add_child(punch_attack)     
 		#print(punch_attack.position)
 		await get_tree().create_timer(0.3).timeout
 		$Sprite2D.texture = FLOOF_PLAYER_PLACE_HOLDER
@@ -95,3 +126,9 @@ func camera_zoom():
 func _on_magnet_area_entered(area: Area2D) -> void:
 	if area.has_method("follow"):
 		area.follow(self)
+
+
+func _on_fist_punch_area_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(attack_damage)
+		body.knockback = attack_direction * 500
